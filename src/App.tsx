a2f73f82a766c71904de8b7902e3bc119804bcc4
Gilbertelnino <gilbeltelnino@gmail.com>
2021-12-10 11:50:41 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import Login from "./components/userauth/Login";
+import Home from "./components/home/Home";
+import { Routes, Route } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useAppDispatch } from "./app/hooks";
+import { useEffect } from "react";
+import { saveUser } from "./features/auth/authSlice";
+import Detail from "./components/details/Detail";
+import Register from "./components/userauth/Register";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { auth } from "./firebase";
+import Visited from "./components/visited/Visited";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userData = {
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          token: user.refreshToken,
+          userId: user.uid,
+        };
+        dispatch(saveUser(userData));
+      } else {
+        signOut(auth).then(() => {
+          navigate("/login");
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<ProtectedRoute component={Home} />} />
+
+        <Route
+          path="/visited"
+          element={<ProtectedRoute component={Visited} />}
+        />
+        <Route
+          path="/details/:id"
+          element={<ProtectedRoute component={Detail} />}
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import countryReducer from "../features/country/countrySlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
+    countries: countryReducer,
   },
 });
 
